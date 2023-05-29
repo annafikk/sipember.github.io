6567c15349614b4130653cc8f8ad1c4c4c1347d0
Cnip! <107799812+annafikk@users.noreply.github.com>
2023-05-30 00:58:36 +0700

    Create bot.js

---
+++ assets/js/bot.js
@@ -0,0 +1,27 @@
+const popup = document.querySelector('.chat-popup');
+const chatBtn = document.querySelector('.chat-btn');
+const submitBtn = document.querySelector('.submit');
+const chatArea = document.querySelector('.chat-area');
+const inputElm = document.querySelector('input');       
+
+
+//   chat button toggler 
+
+chatBtn.addEventListener('click', ()=>{
+    popup.classList.toggle('show');
+})
+
+// send msg 
+submitBtn.addEventListener('click', ()=>{
+    let userInput = inputElm.value;
+
+    let temp = `<div class="out-msg">
+    <span class="my-msg">${userInput}</span>
+    <img src="images/client.jpg" class="avatar">
+    </div>`;
+
+    chatArea.insertAdjacentHTML("beforeend", temp);
+    inputElm.value = '';
+
+})
+
